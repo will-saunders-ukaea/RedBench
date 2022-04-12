2b9c00cca4d99b066143866c6ef9b5ec8b3e4079
Will Saunders <will.saunders@ukaea.uk>
2022-04-12 20:49:40 +0100

    sycl atomic code matches handwritten cuda code when using explicit memory copies

--- README.md
+++ README.md
@@ -3,18 +3,24 @@
 RedBench is a tool to benchmark reductions into elements of arrays using different hardware and programming models. Run ``make`` to run benchmarks.
 
 ```
-$ REDBENCH_NUM_THREADS=8 make
-julia -t 8 src/main.jl
+$ REDBENCH_NUM_THREADS=12 make
+julia -t 12 src/main.jl
 ┌───────────────────────┬───────────┬───────────┬───────────┐
 │                  Name │  min GB/s │ mean GB/S │  max GB/s │
 ├───────────────────────┼───────────┼───────────┼───────────┤
-│            CudaAtomic │   46.1183 │   48.5106 │   49.0813 │
-│     CSequentialNative │   6.11668 │   6.30272 │    6.4308 │
-│        COpenMPReorder │   18.3502 │   18.8766 │   19.0838 │
-│ JuliaSequentialNative │   4.14559 │   4.17225 │   4.18229 │
-│         COpenMPAtomic │  0.428793 │  0.442635 │  0.453462 │
-│     JuliaThreadAtomic │  0.579146 │  0.620691 │  0.652335 │
-│    JuliaThreadReorder │   4.40628 │   10.0248 │   15.9823 │
-│  JuliaSequentialNaive │ 0.0619855 │ 0.0736596 │ 0.0898127 │
+│     CSequentialNative │   5.85993 │    5.9904 │   6.10022 │
+│        COpenMPReorder │   11.7935 │   22.6006 │   27.1062 │
+│ JuliaSequentialNative │   4.11157 │   4.15819 │   4.17597 │
+│         COpenMPAtomic │  0.409949 │  0.442561 │   0.48163 │
+│            SYCLAtomic │  0.679761 │  0.701126 │  0.728298 │
+│     JuliaThreadAtomic │  0.839006 │  0.927923 │   1.05344 │
+│    JuliaThreadReorder │   17.8509 │   19.6762 │    22.068 │
+│  JuliaSequentialNaive │ 0.0536862 │ 0.0686354 │ 0.0873238 │
 └───────────────────────┴───────────┴───────────┴───────────┘
+┌───────────────┬──────────┬───────────┬──────────┐
+│          Name │ min GB/s │ mean GB/S │ max GB/s │
+├───────────────┼──────────┼───────────┼──────────┤
+│    CudaAtomic │  43.2898 │    46.024 │  46.7007 │
+│ SYCLAtomicGPU │  42.8972 │   43.4851 │  44.2888 │
+└───────────────┴──────────┴───────────┴──────────┘
 ```
--- src/sycl_native/SYCLAtomic.cpp
+++ src/sycl_native/SYCLAtomic.cpp
@@ -34,21 +34,32 @@ extern "C" int c_runner(
     }
     
     // std::cout << "Using " << d.get_info<sycl::info::device::name>() << std::endl;
+    
+    sycl::property_list profiling_property{cl::sycl::property::queue::enable_profiling()};
+    sycl::queue Queue(d, profiling_property);
 
-    sycl::queue Queue(d);
 
-    sycl::buffer<int64_t, 1> d_source_indices(source_indices, sycl::range<1>(num_sources * num_components));
-    sycl::buffer<double, 1> d_source_values(source_values, sycl::range<1>(num_sources * num_components));
-    sycl::buffer<double, 1> d_elements(elements, sycl::range<1>(num_elements * num_components));
+    auto d_source_indices = sycl::malloc_device<int64_t>(num_sources * num_components, Queue);
+    auto d_source_values = sycl::malloc_device<double>(num_sources * num_components, Queue);
+    auto d_elements = sycl::malloc_device<double>(num_elements * num_components, Queue);
+
+    auto e0 = Queue.memcpy(d_source_indices, source_indices, num_sources * num_components * sizeof(int64_t));
+    auto e1 = Queue.memcpy(d_source_values, source_values, num_sources * num_components * sizeof(double));
+    auto e2 = Queue.memcpy(d_elements, elements, num_elements * num_components * sizeof(double));
+    e0.wait() ; e1.wait() ; e2.wait();
+
+    //sycl::buffer<int64_t, 1> b_source_indices(source_indices, sycl::range<1>(num_sources * num_components));
+    //sycl::buffer<double, 1> b_source_values(source_values, sycl::range<1>(num_sources * num_components));
+    //sycl::buffer<double, 1> b_elements(elements, sycl::range<1>(num_elements * num_components));
 
     std::chrono::high_resolution_clock::time_point _loop_timer_t0 = std::chrono::high_resolution_clock::now();
     
     Queue.submit(
         [&](sycl::handler& cgh) {
-            auto a_source_indices = d_source_indices.get_access<sycl::access::mode::read>(cgh);
-            auto a_source_values = d_source_values.get_access<sycl::access::mode::read>(cgh);
-            auto a_elements = d_elements.get_access<sycl::access::mode::read_write>(cgh);
-
+            /*
+            auto a_source_indices = b_source_indices.get_access<sycl::access::mode::read>(cgh);
+            auto a_source_values = b_source_values.get_access<sycl::access::mode::read>(cgh);
+            auto a_elements = b_elements.get_access<sycl::access::mode::read_write>(cgh);
             cgh.parallel_for<add_runner>(sycl::range<1>(num_sources), [=](sycl::id<1> idx) {
                 const int64_t index = a_source_indices[idx] - 1;
                 for (int64_t cx=0 ; cx<num_components ; cx++){
@@ -60,21 +71,44 @@ extern "C" int c_runner(
                         sycl::memory_order::relaxed,
                         sycl::memory_scope::device
                     > element_atomic(a_elements[output_index]);
+                    element_atomic.fetch_add(value);
+                }
+            });
+
+            */
+            cgh.parallel_for<add_runner>(sycl::range<1>(num_sources), [=](sycl::id<1> idx) {
+                const int64_t index = d_source_indices[idx] - 1;
+                for (int64_t cx=0 ; cx<num_components ; cx++){
+                    const int64_t output_index = cx * num_elements + index;
+                    const double value = d_source_values[cx * num_sources + idx];
+                    
+                    sycl::atomic_ref<
+                        double,
+                        sycl::memory_order::relaxed,
+                        sycl::memory_scope::device
+                    > element_atomic(d_elements[output_index]);
 
                     element_atomic.fetch_add(value);
 
                 }
             });
+            
+
         }
 
     );
 
     Queue.wait();
 
-
     std::chrono::high_resolution_clock::time_point _loop_timer_t1 = std::chrono::high_resolution_clock::now();
  std::chrono::duration<double> _loop_timer_res = _loop_timer_t1 - _loop_timer_t0;
     *t_internal= (double) _loop_timer_res.count();
+
+    Queue.memcpy(elements, d_elements, num_elements * num_components * sizeof(double)).wait();
+    
+    sycl::free(d_source_indices, Queue);
+    sycl::free(d_source_values, Queue);
+    sycl::free(d_elements, Queue);
 
     return 0;
 }
